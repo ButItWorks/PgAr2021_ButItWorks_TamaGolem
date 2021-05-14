15028bbea0817b3a5bbeb5756a916f4363573856
fedebre <f.braga003@studenti.unibs.it>
2021-05-14 16:23:04 +0200

    Update Equilibrio.java

--- src/Equilibrio.java
+++ src/Equilibrio.java
@@ -61,11 +61,6 @@ public class Equilibrio {
         return elementi.keySet();
     }
 
-    /**
-     * metodo che genera l'equilibrio secondo l'algoritmo che abbiamo pensato
-     * @param elementi
-     * @param vitaGolem
-     */
     private void creaEquilibrio(List<Elemento> elementi, int vitaGolem) {
         int numeroElementi = elementi.size();
         int[][] equilibrio = new int[numeroElementi][numeroElementi];
@@ -124,7 +119,4 @@ public class Equilibrio {
         }
 
     }
-
-
-
 }
